5592049872c54f2f71463e79a35d2ef81d6ac429
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-17 17:51:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,14 @@ class Student
     new_s
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT name FROM students WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
