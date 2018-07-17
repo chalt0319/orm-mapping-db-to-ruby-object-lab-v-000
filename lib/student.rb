e15c2a41704e951fc595f86ae7181cabc9f240f4
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-17 18:11:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
 
     DB[:conn].execute(sql, name).map do |row|
       Student.new_from_db(row)
-    end
+    end.first
 
     # row = DB[:conn].execute(sql)
     # Student.new_from_db(row.flatten)
